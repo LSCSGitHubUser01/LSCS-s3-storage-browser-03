ddd7208bc3cf6d7d182b2679a306378bcdb20807
Louis Scozzari <lscsmbp01@LSCSMBP-01.local>
2025-02-17 09:36:39 -0600

    add username to auth (working)

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function App() {
     <Authenticator>
       {({ signOut, user }) => (
         <main>
-          <h1>Hello {user?.username}</h1>
+          <h1>Hello {user?.attributes?.name || user?.username}</h1>
           <button onClick={signOut}>Sign out</button>
 
           {/* StorageBrowser Component */}
